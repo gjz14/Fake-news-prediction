b1eee98468537af77785d10faba6b5a467608dfe
gjz14 <544402104@qq.com>
2019-06-02 00:26:37 -0700

    add javascript file to modify lime html

---
+++ myjs_script0.js
@@ -0,0 +1,6 @@
+var oImg = document.createElement("img");
+oImg.setAttribute('src', './testimage1.png');
+oImg.setAttribute('alt', 'na');
+oImg.setAttribute('height', '500px');
+oImg.setAttribute('width', '500px');
+document.body.appendChild(oImg);
